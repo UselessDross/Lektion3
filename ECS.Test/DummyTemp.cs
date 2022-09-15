306a7282769d2c155764f09bcc00f673e9250d07
Useless <202010014@post.au.dk>
2022-09-15 11:34:10 +0200

    The tests for the ECS class has been implemented and passes all tests

---
+++ ECS.Test/DummyTemp.cs
@@ -0,0 +1,23 @@
+﻿using ECS.Legacy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Test
+{
+    public class DummyTemp : ITempSensor
+    {
+        public int globalTemp;
+        public DummyTemp(int globalTemp)
+        {
+            this.globalTemp = globalTemp;
+        }
+
+        public int GetTemp()
+        {
+            return globalTemp;
+        }
+    }
+}
--- ECS.Test/UnitTest1.cs
+++ ECS.Test/UnitTest1.cs
@@ -1,19 +1,62 @@
+
 using ECS.Legacy;
 using System;
 namespace ECS.Test
 {
-    public class Tests
+    public class Tests 
     {
+        ECS.Legacy.ECS UUT;
         [SetUp]
         public void Setup()
         {
-            ECS UUT = new ECS();
+            UUT = new(13);
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(4, 4)]
+        [TestCase(5, 5)]
+        [TestCase(11, 11)]
+        public void SetThreshold_TestCorracte(int a, int result)
         {
-            Assert.Pass();
+            UUT.SetThreshold(a);
+            Assert.That(UUT.GetThreshold(), Is.EqualTo(result));
         }
+
+        [TestCase(3, 2, 3)]
+        [TestCase(5, 2, 5)]
+        [TestCase(9, 2, 9)]
+        public void ragolator_overThresholdTestCorrect(int a, int b, int result)
+        {
+            var testheat = new Heater();
+            var TestTemp = new DummyTemp(a);
+            ECS.Legacy.ECS UUT = new(b, TestTemp, testheat);
+            
+            Assert.That(UUT.Regulate(), Is.EqualTo($"Temperatur measured was {result}Heater is off"));
+        }
+
+        [TestCase(3, 9, 3)]
+        [TestCase(5, 9, 5)]
+        [TestCase(7, 9, 7)]
+        public void ragolator_UnderThresholdTestCorrect(int a, int b, int result)
+        {
+            var testheat = new Heater();
+            var TestTemp = new DummyTemp(a);
+            ECS.Legacy.ECS UUT = new(b, TestTemp, testheat);
+
+            Assert.That(UUT.Regulate(), Is.EqualTo($"Temperatur measured was {result}Heater is on"));
+        }
+
+        [TestCase(3, 3)]
+        [TestCase(5,5)]
+        [TestCase(7,7)]
+        public void GetCurTemp_IsCorrect(int a, int result)
+        {
+            var testheat = new Heater();
+            var TestTemp = new DummyTemp(a);
+            ECS.Legacy.ECS UUT = new(5, TestTemp, testheat);
+            
+            Assert.That(UUT.GetCurTemp(), Is.EqualTo(result));
+        }
+
+
     }
 }
